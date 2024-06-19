658ff962c41160a5d363d1b10bb73733e3a75ea7
Ayushi Setiya <116902479+ayushisetiya@users.noreply.github.com>
2024-06-20 00:38:38 +0530

    Add files via upload

---
+++ src/app.config.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCM_AAEDbbi-h9WW9sv5KupXLUIIlzhGo",
+  authDomain: "slack-41c48.firebaseapp.com",
+  projectId: "slack-41c48",
+  storageBucket: "slack-41c48.appspot.com",
+  messagingSenderId: "326747621985",
+  appId: "1:326747621985:web:e67694cc77b5c3d804b820",
+  measurementId: "G-XS43437S3L"
+};
+
+const cometChat = {
+  APP_ID: '2595285b0ef8b1ba',
+  AUTH_KEY: 'd563958cfc5415c44ae42b71cb5cf52ad9e79a31',
+  REST_KEY: '8821db47a4091e5d784086de1d087906791181bc',
+  APP_REGION: 'in',
+}
+export { firebaseConfig, cometChat }
